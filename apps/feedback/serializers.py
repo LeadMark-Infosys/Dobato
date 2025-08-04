67fe16c448afa2fb04dd0a8591f384df19a380a8
ahmadsaif12 <ahmadsaif12>
2025-08-04 12:01:41 +0545

    updated in feedback app

--- apps/feedback/serializers.py
+++ apps/feedback/serializers.py
@@ -1,24 +1,16 @@
 from rest_framework import serializers
-from .models import Feedback, FeedbackCategory, QRLocation, FeedbackStatus
-
-class FeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = '__all__'
-
-    def validate_approved_by(self, value):
-        if value and value.user_type != 'municipality_admin':
-            raise serializers.ValidationError("Only municipality_admin can approve feedback.")
-        return value
-
+from .models import (
+    Feedback,
+    FeedbackCategory,
+    QRLocation,
+    FeedbackStatus,
+    FeedbackResponse,
+    Attachment,
+    QRAnalytics
+)
 class FeedbackCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = FeedbackCategory
-        fields = '__all__'
-
-class QRLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QRLocation
         fields = '__all__'
 
 class FeedbackStatusSerializer(serializers.ModelSerializer):
@@ -26,18 +18,35 @@ class FeedbackStatusSerializer(serializers.ModelSerializer):
         model = FeedbackStatus
         fields = '__all__'
 
-class FeedbackResponseSerializer(serializers.ModelSerializer):
+class QRLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QRLocation
+        fields = '__all__'
+
+class FeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feedback
         fields = '__all__'
-
-    def validate_responder(self, value):
+    def validate_approved_by(self, value):
         if value and value.user_type != 'municipality_admin':
-            raise serializers.ValidationError("Only municipality_admin can respond to feedback.")
+            raise serializers.ValidationError("Only municipality_admin can approve feedback.")
+        return value
+
+class FeedbackResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FeedbackResponse
+        fields = '__all__'
+    def validate_responder(self, value):
+        if value and value.user_type not in ['municipality_admin', 'department_manager']:
+            raise serializers.ValidationError("Only municipality_admin or department_manager can respond to feedback.")
         return value
 
 class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Feedback
-        fields = ['qr_code_image']  
-        read_only_fields = ['qr_code_image']
+        model = Attachment
+        fields = '__all__'
+
+class QRAnalyticsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QRAnalytics
+        fields = '__all__'
